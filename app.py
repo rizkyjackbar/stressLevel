fd259d7d8816d5fda50b1f1e14ecebb212caa4cb
rizkyjackbar <muhamadrizky62020@students.amikom.ac.id>
2024-06-17 00:12:39 +0700

    test deploy to vercel

--- app.py
+++ app.py
@@ -57,4 +57,4 @@ def predict():
     return jsonify(response)
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8000)))
